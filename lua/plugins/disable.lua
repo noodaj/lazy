e223d3cfdb568f0c77f43873b66dbce65a91a0bd
jadon <jadon.loi.68@gmail.com>
2024-04-28 02:03:53 -0700

    pog x3

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -3,4 +3,5 @@ return {
   { "echasnovski/mini.animate", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
   { "RRethy/vim-illuminate", enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,9 +5,34 @@ return {
       position = "left",
       width = 25,
     },
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          --change
+          added = "✚",
+          modified = "★",
+          deleted = " ",
+          renamed = "➜",
+          --status
+          untracked = "★",
+          unstaged = "✗",
+          staged = "✓",
+          ignored = "◌",
+        },
+      },
+    },
   },
   keys = {
-    { "<leader>e", desc = "Open file tree", remap = true },
+    -- disabled
     { "<leader>E", enabled = false },
+    -- enabled
+    {
+      "<leader>e",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+      end,
+      desc = "Open file tree",
+      remap = true,
+    },
   },
 }
