0e93b35171e4ed3f840e2b225dc1061d09312731
jadon <jadon.loi.68@gmail.com>
2024-05-14 23:43:57 -0700

    repog x2

--- lua/plugins/nvimcmp.lua
+++ lua/plugins/nvimcmp.lua
@@ -4,7 +4,26 @@ return {
     opts = function(_, opts)
       local cmp = require("cmp")
 
-      cmp.cmp.setup({
+      cmp.setup({
+        formatting = {
+          fields = {
+            "kind",
+            "abbr",
+            "menu",
+          },
+          expandable_indicator = false,
+          format = function(entry, item)
+            local icons = require("lazyvim.config").icons.kinds
+            if icons[item.kind] then
+              item.menu = " "
+                .. (({ nvim_lsp = "lsp", cmp_git = "git" })[entry.source.name] or entry.source.name)
+                .. " : "
+                .. item.kind
+              item.kind = nil
+            end
+            return item
+          end,
+        },
         mapping = {
           ["<CR>"] = cmp.config.disable,
           ["<Tab>"] = cmp.mapping.confirm({ select = true }),
